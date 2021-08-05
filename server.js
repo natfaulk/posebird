9619db589d162cc91c24b10dd674cfe3da9824df
natfaulk <nfaulk@hotmail.co.uk>
2021-08-05 16:18:39 +0100

    make server production ready (ish....)

---
+++ server.js
@@ -0,0 +1,28 @@
+/* eslint-env node */
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const SETTINGS_FILE = 'settings.json'
+
+const app = express()
+let port = 3000
+
+app.use('/public', express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+if (fs.existsSync(SETTINGS_FILE)) {
+  const settingsraw = fs.readFileSync(SETTINGS_FILE, 'utf8')
+  try {
+    const settings = JSON.parse(settingsraw)
+    if (typeof(settings.port) === 'number') port = settings.port
+  } catch (e) {
+    console.log('Invalid settings.json file. Using defaults')
+  }
+} else console.log('No settings.json file. Using defaults')
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
