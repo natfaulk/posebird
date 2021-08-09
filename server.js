a400dea209b0a6b8ce88ae068e4d2a061b06fb39
natfaulk <nfaulk@hotmail.co.uk>
2021-08-09 11:57:32 +0100

    don't cache files in dev mode

--- server.js
+++ server.js
@@ -2,27 +2,33 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const lg = require('@natfaulk/supersimplelogger')('Server')
 
 const SETTINGS_FILE = 'settings.json'
 
 const app = express()
 let port = 3000
-
-app.use('/public', express.static('public', {maxAge: '7d'}))
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+let dev = false
 
 if (fs.existsSync(SETTINGS_FILE)) {
   const settingsraw = fs.readFileSync(SETTINGS_FILE, 'utf8')
   try {
     const settings = JSON.parse(settingsraw)
     if (typeof(settings.port) === 'number') port = settings.port
+    if (settings.dev === true) dev = true
   } catch (e) {
-    console.log('Invalid settings.json file. Using defaults')
+    lg('Invalid settings.json file. Using defaults')
   }
-} else console.log('No settings.json file. Using defaults')
+} else lg('No settings.json file. Using defaults')
 
+// disable caching on dev mode
+const staticConfig = {}
+if (!dev) staticConfig.maxAge = '7d'
+
+app.use('/public', express.static('public', staticConfig))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  lg(`App listening on port ${port}`)
 })
